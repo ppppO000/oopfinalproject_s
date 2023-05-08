6346375d90ac56c29215ea06f957875c3a9c49e6
b10505043 <b10505043@ntu.edu.tw>
2023-05-08 18:48:00 +0800

    co

--- oopproject/src/oopproject/test.java
+++ oopproject/src/oopproject/test.java
@@ -6,9 +6,6 @@ public class test {
 		// TODO Auto-generated method stub
 		Book a=new Book("a","b","5");
 		Book b=new Book("c","b","5");
-		Book c=new Book("D","b","5");
-		Book d=new Book("e","b","5");
-		Book n=new Book("e","b","5");
 		Library x=new Library();
 		x.addBook(a);
 		x.addBook(b);
