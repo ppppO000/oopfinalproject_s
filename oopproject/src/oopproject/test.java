a0493a75a18174076fff6013a2e7ef74919c49e9
Ariel <Ariel@SX14_RICK>
2023-05-08 17:45:21 +0800

    hi

---
+++ oopproject/src/oopproject/test.java
@@ -0,0 +1,15 @@
+package oopproject;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Book a=new Book("a","b","5");
+		Book b=new Book("c","b","5");
+		Library x=new Library();
+		x.addBook(a);
+		x.addBook(b);
+		System.out.println(x.getBooks());
+	}
+
+}
